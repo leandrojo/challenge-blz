d604a81016dfeacea1c6d048862ec978c3dd8db8
Leandro <contato@leandroaraujo.me>
2020-05-26 21:25:40 -0300

    feat(components): Create view component for values.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,3 @@
-import Head from 'next/head';
+import Cart from './cart';
 
-import { Steps } from '../components';
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Challenge – Beleza na Web</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Steps />
-    </div>
-  )
-}
+export default Cart;

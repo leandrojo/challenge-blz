30da60aaf8bbe053d662e15ab2894ee31fd54d31
Leandro <contato@leandroaraujo.me>
2020-05-26 21:03:35 -0300

    feat(components): Create basics components.

---
+++ components/Card/Card.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background-color: #FFF;
+  border-radius: 3px;
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 29, 0.22);
+  padding: 12px;
+`;
+
+export default Card;
--- theme.tsx
+++ theme.tsx
@@ -4,13 +4,18 @@ const colors = {
   primaryLight: '#FF7800',
   primary: '#FF6C00',
   primaryDark: '#D45A00',
-  gray: '#999',
-  grayLight: '#CCC',
+  secondary: '#A43287',
   grayExtraLight: '#EEE',
+  grayLight: '#E0E7EE',
+  gray: '#CCC',
+  grayDark: '#999',
+  grayExtraDark: '#212122',
+  danger: '#F30',
 }
 
 const theme = {
   backgroundColor: colors.grayExtraLight,
+  fontFamily: '"Helvetica Neue", sans-serif',
   colors,
 };
 

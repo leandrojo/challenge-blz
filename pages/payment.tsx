0a6fa576586c240fff4c7535d76c27f64af9feae
Leandro <contato@leandroaraujo.me>
2020-05-26 21:37:30 -0300

    feat(page): Include payment page, second step.

---
+++ pages/payment.tsx
@@ -0,0 +1,43 @@
+import { useCallback, useEffect } from 'react';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import { Button, Card, CreditCardFormController, CreditCardForm, Heading, Layout, Total } from '../components';
+import { useCart, useCreditCard } from '../enhancers';
+
+const Payment: NextPage = () => {
+  const router = useRouter();
+  const { data } = useCart();
+  const { save } = useCreditCard();
+
+  const handleSubmit = useCallback(async (payload: any) => {
+    await save(payload);
+    router.push('/success');
+  }, [save]);
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Pagamento – Challenge – Beleza na Web</title>
+      </Head>
+
+      <Layout>
+        <CreditCardFormController onSubmit={handleSubmit}>
+          {() => (
+            <>
+              <Heading>Cartão de Crédito</Heading>
+              <Card>
+                <CreditCardForm />
+              </Card>
+              <Total data={data} />
+              <Button fluid type="submit">Finalizar o Pedido</Button>
+            </>
+          )}
+        </CreditCardFormController>
+      </Layout>
+    </div>
+  )
+}
+
+export default Payment;
